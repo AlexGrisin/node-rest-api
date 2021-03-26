f3e1fc10050488f5d02f3c8a7642b70ad3901112
Alexandr Grisin <agrisin@tacitknowledge.com>
2021-03-26 18:14:51 +0200

    MongoDB integration

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
 const express = require("express");
-const app = express();
-var fs = require("fs");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
 
+// const CONNECTION_URL = 'mongodb://localhost/resthub';
+const CONNECTION_URL = "mongodb://87.251.71.72:27017";
+
+const DATABASE_NAME = "test";
 const PORT = 8000;
-const HOST = "0.0.0.0";
 
-__dirname = "resources";
+const app = express();
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.listen(PORT, () => {
+  MongoClient.connect(
+    CONNECTION_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (error, client) => {
+      if (error) {
+        throw error;
+      }
+      database = client.db(DATABASE_NAME);
+      collection = database.collection("users");
+      console.log("Connected to `" + DATABASE_NAME + "`!");
+    }
+  );
+});
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.get("/list", function (req, res) {
-  fs.readFile(__dirname + "/" + "users.json", "utf8", function (err, data) {
-    console.log(data);
-    res.end(data);
+app.get("/users", (request, response) => {
+  collection.find({}).toArray((error, result) => {
+    if (error) {
+      return response.status(500).send(error);
+    }
+    response.send(result);
   });
 });
 
-app.get("/:id", function (req, res) {
-  // First read existing users.
-  fs.readFile(__dirname + "/" + "users.json", "utf8", function (err, data) {
-    var users = JSON.parse(data);
-
-    var filteredObject = Object.keys(users).reduce(function (r, e) {
-      if (users[e].id.toString() === req.params.id.toString()) {
-        r = users[e];
-      }
-      return r;
-    }, {});
-
-    res.end(JSON.stringify(filteredObject));
+app.get("/users/:id", (request, response) => {
+  collection.findOne({ _id: new ObjectId(request.params.id) }, (error, result) => {
+    if (error) {
+      return response.status(500).send(error);
+    }
+    response.send(result);
   });
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.post("/users/add", (request, response) => {
+  collection.insert(request.body, (error, result) => {
+    if (error) {
+      return response.status(500).send(error);
+    }
+    response.send(result.result);
+  });
+});
