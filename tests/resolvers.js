f75ec6e98dd1e2df244889c6ffad2b851758a151
Alexandr Grisin <agrisin@tacitknowledge.com>
2021-04-25 20:16:32 +0300

    Add tests

---
+++ tests/resolvers.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+const url = "https://icanhazdadjoke.com/";
+const headers = { Accept: "application/json" };
+const resolvers = {
+  Query: {
+    joke: () =>
+      axios({ method: "GET", url, headers }).then(({ data: { joke } }) => ({
+        text: joke,
+      })),
+  },
+};
+module.exports = resolvers;
---
+++ tests/type-defs.js
@@ -0,0 +1,10 @@
+const { gql } = require("apollo-server");
+const typeDefs = gql`
+  type Joke {
+    text: String!
+  }
+  type Query {
+    joke: Joke!
+  }
+`;
+module.exports = typeDefs;
